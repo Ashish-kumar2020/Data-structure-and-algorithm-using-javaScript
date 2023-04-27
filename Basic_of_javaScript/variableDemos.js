0dcccd5dc445e22042846dbb6c6614cabb172eb6
Ashish Kumar Singh <ashishsinghk2020@gmail.com>
2023-04-27 14:29:10 +0530

    variables

---
+++ Basic_of_javaScript/variableDemos.js
@@ -0,0 +1,12 @@
+// creating variables
+
+// using var keyword
+var score = 10;
+var marks = 20;
+
+// using let keyword
+let age = 23;
+let flag = 0;
+
+// using const keyword
+const houseNumber = 12;
