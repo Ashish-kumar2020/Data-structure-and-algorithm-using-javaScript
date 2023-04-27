9feaec31790e6147426bc0168af508f21478afac
Ashish Kumar Singh <ashishsinghk2020@gmail.com>
2023-04-27 22:34:48 +0530

    operators

---
+++ Basic_of_javaScript/bitwiseOperator.js
@@ -0,0 +1,5 @@
+// bitwise operator
+
+console.log(5 & 7);
+console.log(6 & 9);
+console.log(5 | 6);
--- Basic_of_javaScript/operatorsDemo.js
+++ Basic_of_javaScript/operatorsDemo.js
@@ -14,3 +14,11 @@ console.log(false || false);
 console.log(true && true);
 console.log((5 < 10) && (6 < 3));
 console.log(!(3 > 4));
+
+console.log(10 && 6);
+console.log(6 || 0);
+console.log(0 || -0);
+console.log(undefined / null);
+console.log(typeof(NaN));
+console.log(NaN === NaN);
+console.log(0 === -0);
