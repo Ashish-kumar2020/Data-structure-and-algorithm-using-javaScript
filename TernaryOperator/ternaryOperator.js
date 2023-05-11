60e8f43bc62e3cdae0a764f563b4f144bf225817
Ashish Kumar Singh <ashishsinghk2020@gmail.com>
2023-05-11 08:25:48 +0530

    ternary operator

---
+++ TernaryOperator/ternaryOperator.js
@@ -0,0 +1,8 @@
+// let y = ((condition) ? (expression 1) : (expression 2));
+
+
+let a = ((10 > 5) ? ("Greater") : ("Less"));
+console.log(a);
+
+let b = ((10 < 5) ? ("Greater") : ("Less"));
+console.log(b);
